4a12e957f8936e2d8b4f003c5220bef2d48ff5aa
edakizak <edakizak.de@gmail.com>
2024-02-05 14:02:56 +0100

    User Story 1 - test done

--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "77d55e24caea4cf0"; }
+/******/ 	__webpack_require__.h = function() { return "bddbf54bf9c7bfef"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
