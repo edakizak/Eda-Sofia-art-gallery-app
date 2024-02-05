9c7bea02f87560c278d18132cfd6440f135cc4f4
edakizak <edakizak.de@gmail.com>
2024-02-05 11:10:16 +0100

    user story 1 - fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
+import useSWR from "swr";
+
+const url = "https://example-apis.vercel.app/api/art";
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(url);
+  console.log(data);
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error!</h1>;
   return (
     <div>
       <h1>Hello from Next.js</h1>
