f3d9ba87841c918d416b5c525f2e665690ebe26a
Sofia Cavelius <sofiacavelius@web.de>
2024-02-05 17:04:09 +0100

    done Userstory 3

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,21 @@
-import useSWR from "swr";
-import ArtPieces from "../components/art_pieces/index.js";
-import Link from "next/link";
+// import ArtPieces from "../components/art_pieces/index.js";
+// import Link from "next/link";
 import Spotlight from "@/components/spotlight/index.js";
 
-const url = "https://example-apis.vercel.app/api/art";
-export default function HomePage() {
-  const { data, error, isLoading } = useSWR(url);
-  console.log(data);
-  if (isLoading) return <h1>Loading...</h1>;
-  if (error) return <h1>Error!</h1>;
-
+export default function SpotlightPage({ data }) {
   const randomPiece = data[Math.floor(Math.random() * data.length - 1)];
   console.log(randomPiece);
+  const pieces = { data };
   return (
     <div>
       <Spotlight
         image={randomPiece.imageSource}
         artist={randomPiece.artist}
         name={randomPiece.name}
+        pieces={data}
       />
-      <ArtPieces pieces={data} />
-      <Link href="/artpieces"> Art Pieces</Link>
+      {/* <ArtPieces pieces={data} />
+      <Link href="/artpieces"> Art Pieces</Link> */}
     </div>
   );
 }
